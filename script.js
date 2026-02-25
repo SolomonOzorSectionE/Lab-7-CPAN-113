7885d7229211d635cbf71d42875208a887d3ffc2
SolomonOzor <157052930+SolomonOzorSectionE@users.noreply.github.com>
2026-02-25 16:49:56 -0500

    Add mouse event listeners for hover button
    event handling

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const hoverBtn = document.getElementById("hoverBtn");
+const mouseMsg = document.getElementById("mouseMsg");
 
+hoverBtn.addEventListener("mouseenter", () => {
+  mouseMsg.textContent = "MOUSE IS ON THE BUTTON I REPEAT, THE MOUSE IS ON THE BUTTON.";
+});
+
+hoverBtn.addEventListener("mouseleave", () => {
+  mouseMsg.textContent = "MOUSE IS NOT ON THE BUTTON ANYMORE, I REPEAT, THE MOUSE IS NOT ON THE BUTTON ANYMORE.";
+});
